9cc9fa6fe9e4592c7a34ea86fe7c398c10242a19
Alexandre Sevilha <alexandre.sevilha1@gmail.com>
2025-10-19 18:55:41 -0300

    Initial commit: Project structure and Product/Category entities

---
+++ EcommerceApp/Models/CategoryModel.cs
@@ -0,0 +1,11 @@
+﻿namespace EcommerceApp.Models
+{
+    public class CategoryModel
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+
+        public ICollection<ProductModel> Produtos { get; set; } = new List<ProductModel>();
+    }
+}
